f927031d60bb9080a393e3841c973a719f78a445
Kevin <mybaseball52@gmail.com>
2019-07-22 01:19:43 +0800

    split loop(267)

--- statement.js
+++ statement.js
@@ -1,19 +1,17 @@
 let plays = {
-    "hamlet": {"name": "Hamlet", "type": "tragedy"},
-    "as-like": {"name": "As You like It", "type": "comedy"},
-    "othello": {"name": "Othello", "type": "tragedy"}
-}
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" }
+};
 
-let invoice = 
-    {
-        "customer": "BigCo",
-        "performances": [
-            {"playID": "hamlet", "audiance": "55"},
-            {"playID": "as-like", "audiance": "35"},
-            {"playID": "othello", "audiance": "40"}
-        ]
-    }
-
+let invoice = {
+  customer: "BigCo",
+  performances: [
+    { playID: "hamlet", audiance: "55" },
+    { playID: "as-like", audiance: "35" },
+    { playID: "othello", audiance: "40" }
+  ]
+};
 
 function statement(invoice, plays) {
   let totalAmount = 0;
@@ -21,14 +19,16 @@ function statement(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    //add volume credit
-    volumeCredits += volumeCreditsFor(perf);
-
     //print this order
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audiance
     } seats)\n`;
     totalAmount += amountFor(perf);
+  }
+
+  for (let perf of invoice.performances) {
+    //add volume credit
+    volumeCredits += volumeCreditsFor(perf);
   }
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
@@ -37,47 +37,48 @@ function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(aPerformance){
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-                result = 40000;
-          if(aPerformance.audiance > 30){
-            result += 1000 * (aPerformance.audiance - 30)
-          }
-          break;
-        case "comedy":
-                result = 30000;
-              if(aPerformance.audiance > 20){
-                result += 1000 + 500 * (aPerformance.audiance - 30)
-              } 
-              result += 300 * aPerformance.audiance;
-          break;
-        default:
-          throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
+function amountFor(aPerformance) {
+  let result = 0;
+  switch (playFor(aPerformance).type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audiance > 30) {
+        result += 1000 * (aPerformance.audiance - 30);
       }
-    return result;
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audiance > 20) {
+        result += 1000 + 500 * (aPerformance.audiance - 30);
+      }
+      result += 300 * aPerformance.audiance;
+      break;
+    default:
+      throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
+  }
+  return result;
 }
 
-function playFor(aPerformance){
-    return plays[aPerformance.playID];
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
 }
 
-function volumeCreditsFor(aPerformance){
-    let result = 0;
-    result += Math.max(aPerformance.audiance - 30, 0);
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audiance - 30, 0);
 
-    //each 10 comedy audiance can get extra points
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audiance / 5);
-    return result;
+  //each 10 comedy audiance can get extra points
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audiance / 5);
+  return result;
 }
 
-function usd(aNumber){
-    return  new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format(aNumber / 100);
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber / 100);
 }
 
 console.log(statement(invoice, plays));

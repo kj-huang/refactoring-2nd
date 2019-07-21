1df7a5dd7653423fdc5d05875c12962a951d5641
Kevin <mybaseball52@gmail.com>
2019-07-21 12:33:24 +0800

    add data and add logic to switch function

--- statement.js
+++ statement.js
@@ -1,3 +1,20 @@
+let plays = {
+    "hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "as-like": {"name": "As You like It", "type": "comedy"},
+    "othello": {"name": "Othello", "type": "tragedy"}
+}
+
+let invoice = 
+    {
+        "customer": "BigCo",
+        "performances": [
+            {"playID": "hamlet", "audiance": "55"},
+            {"playID": "as-like", "audiance": "35"},
+            {"playID": "othello", "audiance": "40"}
+        ]
+    }
+
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -14,8 +31,17 @@ function statement(invoice, plays) {
 
     switch (play.type) {
       case "tragedy":
+        thisAmount = 40000;
+        if(perf.audiance > 30){
+            thisAmount += 1000 * (perf.audiance - 30)
+        }
         break;
       case "comedy":
+            thisAmount = 30000;
+            if(perf.audiance > 20){
+                thisAmount += 1000 + 500 * (perf.audiance - 30)
+            } 
+            thisAmount += 300 * perf.audiance;
         break;
       default:
         throw new Error(`Unknown type: ${play.type}`);
@@ -36,6 +62,8 @@ function statement(invoice, plays) {
 
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
-  
+
   return result;
 }
+
+console.log(statement(invoice, plays));

1382f7ff7e85eed4e8f3fc30ab931a565936f87b
Kevin <mybaseball52@gmail.com>
2019-07-22 23:38:41 +0800

    split functions to seperate files

--- statement.js
+++ statement.js
@@ -1,9 +1,3 @@
-let plays = {
-  hamlet: { name: "Hamlet", type: "tragedy" },
-  "as-like": { name: "As You like It", type: "comedy" },
-  othello: { name: "Othello", type: "tragedy" }
-};
-
 let invoice = {
   customer: "BigCo",
   performances: [
@@ -33,64 +27,8 @@ function renderPlainText(data) {
 }
 
 function statement(invoice) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  statementData.totalAmounts = totalAmounts(statementData);
-  return renderPlainText(statementData);
-
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(aPerformance);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audiance > 30) {
-          result += 1000 * (aPerformance.audiance - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audiance > 20) {
-          result += 1000 + 500 * (aPerformance.audiance - 30);
-        }
-        result += 300 * aPerformance.audiance;
-        break;
-      default:
-        throw new Error(`Unknown type: ${data.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audiance - 30, 0);
-
-    //each 10 comedy audiance can get extra points
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audiance / 5);
-    return result;
-  }
-
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
-
-  function totalAmounts(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
+  let createStatementData = require("./createStatementData.js");
+  return renderPlainText(createStatementData(invoice));
 }
 
 console.log(statement(invoice));
